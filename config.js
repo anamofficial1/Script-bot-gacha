105e2ff87fa4564b12d154e550506ed69d2bc1e3
Anam Anonymous <anamtradervx@gmail.com>
2025-12-28 00:40:37 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,31 @@
+/*
+
+   BASE INI FREE NO SALE
+   JIKA MENEMUKAN YANG MENJUAL BELIKAN
+   HARAP LAPORKAN KE 
+   DEVELOPER : https://t.me/angkasanybobo
+   CHANNEL : https://t.me/angkasalagibobo
+   ROOMPUBLIC : https://t.me/roomangkasa
+   KEBUTUHAN HOSTING : https://angkasalagijajan.vercel.app
+
+*/
+
+module.exports = {
+  VERSION: "0.7",
+  TOKEN: "8360631998:AAFivlV8JqKgdfFr3AGPdDOuVTFE3zDCNeo",
+  OWNER: 6736986837,
+  NAMA_OWNER: "ANGKASA",
+  USERNAME_BOT: "basenyabot",
+  NAMA_BOT: "BOT ANGKASA FIX",
+  APIKEY: "ANGKASALAGIBOBO",
+  CHANNEL_ID: "-1003184002609",
+  CHANNEL_LINK: "https://t.me/inikanangkasa",
+  GETCHANNELID_USERNAME: "@inikanangkasa"
+  
+};
+
+/*
+
+   GROUP_ID SESUAI KAN DENGAN GROUP MASING MASING
+
+*/
